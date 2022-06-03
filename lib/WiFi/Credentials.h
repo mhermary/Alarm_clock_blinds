dea78add3a5b7fd50b7daeb3077147a1bc2ded8e
Matthew Hermary <matthermary@gmail.com>
2022-06-02 18:21:00 -0700

    removed

--- lib/WiFi/Credentials.h
+++
@@ -1,7 +0,0 @@
-const char* MyNetwork = "YOURE WIFI SSID";
-const char* MyPassword = "YOUR WIFI PASSWORD";
-const char* mqtt_user = "FlespiToken YOUR FLESPIO TOKEN";
-const char* mqtt_pass = "";
-const char* mqtt_broker = "mqtt.flespi.io";
-const char* mqtt_outTopic ="YOUR BOARD NAME FOR OUTGOING MESSAGES";
-const char* mqtt_inTopic = "YOUR BOARD NAME FOR INCOMING MESSAGES";
